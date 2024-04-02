a71673ecb65f6ccd25c184d8c58d9e06f731d46e
Bernardo Vieira <19441097+obernardovieira@users.noreply.github.com>
2024-04-02 19:48:36 +0100

    fix envs

--- app/config.ts
+++ app/config.ts
@@ -1,3 +1,3 @@
 // use NODE_ENV to not have to change config based on where it's deployed
-export const NEXT_PUBLIC_URL = process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://hyperion.vercel.app';
+export const NEXT_PUBLIC_URL = process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : process.env.NEXT_PUBLIC_URL;
 export const NEXT_IS_PROD = process.env.NODE_ENV === 'production';
